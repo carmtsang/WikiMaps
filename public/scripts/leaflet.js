5b9f751f94645c66918030869698b295b5296651
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-24 20:58:30 +0000

    debug map - simple map now loads

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -1,10 +1,10 @@
-const map = L.map('map').setView([51.505, -0.09], 130);
+// initialize a map
+let map = L.map('map').setView([51.505, -0.09], 13);
 
+// load a tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '© OpenStreetMap'
 }).addTo(map);
 
-module.exports = {
-  map
-}
+const marker = L.marker([51.5, -0.09]).addTo(map);
