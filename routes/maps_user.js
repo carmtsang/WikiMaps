ef8d69a3ba266606ddf76be75527f22fd34850bb
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-25 19:59:36 +0000

    renamed maps to maps_user

--- routes/maps_user.js
+++ routes/maps_user.js
@@ -3,28 +3,12 @@ const router  = express.Router();
 
 
 module.exports = (db) => {
-<<<<<<< HEAD
-  router.get("/maps", (req, res) => {
-    res.render("maps")
-=======
-  router.get('/maps', (req, res) => {
+  router.get('/maps_user', (req, res) => {
     res.render('maps')
->>>>>>> master
 
   db.query(`SELECT * FROM maps;`)
     .then(data => {
       const maps = data.rows;
-<<<<<<< HEAD
-      res.json({ maps });
-    })
-    .catch(err => {
-      res
-        .status(500)
-        .json({ error: err.message });
-    });
-
-
-=======
        res.json({ maps });
       })
       .catch(err => {
@@ -32,7 +16,6 @@ module.exports = (db) => {
           .status(500)
           .json({ error: err.message });
       });
->>>>>>> master
   });
   return router;
 };
