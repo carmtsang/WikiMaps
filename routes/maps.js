61cf7a6d44044ff1d4aa2412cab1c591a817a123
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-25 22:34:55 +0000

    update to render maps_user

--- routes/maps.js
+++ routes/maps.js
@@ -3,12 +3,8 @@ const router  = express.Router();
 
 
 module.exports = (db) => {
-<<<<<<< HEAD
   router.get('/maps', (req, res) => {
-=======
-  router.get('/maps_user', (req, res) => {
->>>>>>> ec010f0e48131f412b5813a948d3a347514c9bda
-    res.render('maps')
+    res.render('maps_user')
 
   db.query(`SELECT * FROM maps;`)
     .then(data => {
