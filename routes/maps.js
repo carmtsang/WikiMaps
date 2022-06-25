15ebf75b7c0727351ad4176ceed1453f699fa0d4
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-25 18:19:32 +0000

    add route to /maps

--- routes/maps.js
+++ routes/maps.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const router  = express.Router();
 
+
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM maps;`)
-      .then(data => {
-        const maps = data.rows;
-        res.json({ maps });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  router.get("/maps", (req, res) => {
+    res.render("maps")
+
+  db.query(`SELECT * FROM maps;`)
+    .then(data => {
+      const maps = data.rows;
+      res.json({ maps });
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+
+
   });
   return router;
 };
