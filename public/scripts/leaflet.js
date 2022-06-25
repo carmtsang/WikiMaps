8f9f6e19cf33b770896e4f73a500ff3c4c552028
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-25 00:44:42 +0000

    add axios

--- public/scripts/leaflet.js
+++ public/scripts/leaflet.js
@@ -1,5 +1,7 @@
+const axios = require('axios').default;
+
 // initialize a map
-let map = L.map('map').setView([49.1836, -123.1164], 13);
+let map = L.map('map').setView([49.238665712, -123.108999564], 13);
 
 // load a tile layer
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
