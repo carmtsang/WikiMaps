68b9704af9042d780be6e8abbaefc8ed926a30e9
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-27 22:48:38 +0000

    start scripts for /maps

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,31 @@
 // Client facing scripts here
+
+
+$(() => {
+
+  const renderMap => maps => {
+    for ()
+  }
+
+  // function to prevent XSS
+  const escape = str => {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  const addNewMapElement = info => {
+    // const mapName = escape(...);
+    // const description = escape(...);
+
+    return `<div class="user_map_list">
+      <div class="map-name-description">
+        <a href="/maps/id">${mapName}</a>
+        <p>${description}</p>
+      </div>
+      <form action="/maps/:${id}/:delete" method="post">
+        <button class="button" type="submit"><i class="fa-solid fa-trash-can"></i></button>
+      </form>
+      </div>`;
+    };
+});
