8b23cb55118577b18c97599409fee594a57d2069
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-28 04:28:46 +0000

    updated, created map.ejs

--- routes/maps.js
+++ routes/maps.js
@@ -5,17 +5,18 @@ const router  = express.Router();
 
 
 module.exports = (db) => {
-  router.get("/", (req, res) => {
-    db.query(`SELECT * FROM maps;`)
-      .then(data => {
-        const users = data.rows;
-        res.json({ users });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+  router.get("/maps", (req, res) => {
+    res.render('maps');
+    // db.query(`SELECT * FROM maps;`)
+    //   .then(data => {
+    //     const users = data.rows;
+    //     res.json({ users });
+    //   })
+    //   .catch(err => {
+    //     res
+    //       .status(500)
+    //       .json({ error: err.message });
+    //   });
   });
   return router;
 
