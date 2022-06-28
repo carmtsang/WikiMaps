b65c81fe4d5017d48e3aa7f4103d9c2083e71a57
roop <rplawn@icloud.com>
2022-06-27 17:06:15 -0700

    Pair programming creating remainder databases + data in schema and seeds files

--- db/schema/03_locations.sql
+++ db/schema/03_locations.sql
@@ -1,5 +1,8 @@
 DROP TABLE IF EXISTS locations CASCADE;
 CREATE TABLE locations (
   id SERIAL PRIMARY KEY NOT NULL,
-  gps_coordinates
+  gps_coordinates INTEGER,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  map_id INTEGER REFERENCES maps(id) on DELETE CASCADE
 );
