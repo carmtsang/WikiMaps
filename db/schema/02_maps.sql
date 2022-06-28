7bfd59ec33755eaa34a625b0ffb47d81eddac468
roop <rplawn@icloud.com>
2022-06-27 20:38:06 -0700

    finalizing data points

--- db/schema/02_maps.sql
+++ db/schema/02_maps.sql
@@ -3,8 +3,6 @@ CREATE TABLE maps (
   id SERIAL PRIMARY KEY NOT NULL,
   description TEXT,
   name VARCHAR(255),
-  comments TEXT,
   owner_id INTEGER REFERENCES users(id) on DELETE CASCADE,
-  contributor_id INTEGER REFERENCES users(id) on DELETE CASCADE,
   like_id
 );
