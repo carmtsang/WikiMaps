3d7d5856b75326b66b409f17c98b9c9331ca0d62
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-29 03:05:15 +0000

    add more seed data

--- db/seeds/03_locations.sql
+++ db/seeds/03_locations.sql
@@ -6,7 +6,16 @@ INSERT INTO locations (
   VALUES (49.284256509181766, -123.10930776074136, 'Timber Train', 'great poor over coffee', 1, 1);
 INSERT INTO locations (
   longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.281792317336965, -123.08540391479706, 'Prototype Coffee', 'nice spot', 2, 1 );
+  VALUES (49.281792317336965, -123.08540391479706, 'Prototype Coffee', 'nice spot', 2, 1);
+INSERT INTO locations (
+  longitude, latitude, name, description, owner_id, map_id)
+  VALUES (49.29680744650704, -123.13565840392819, 'Stanley Park', '50/50', 3, 2);
+INSERT INTO locations (
+  longitude, latitude, name, description, owner_id, map_id)
+  VALUES (49.16864715256868, -122.57992336032277, 'Fort Langley', 'X-Files', 3, 2);
+INSERT INTO locations (
+  longitude, latitude, name, description, owner_id, map_id)
+  VALUES (49.238665712, -123.108999564, 'Queen Elizabeth Park', 'The Unforgivable', 3, 2);
 
 
 
