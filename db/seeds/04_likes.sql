8e54774ead7a276a8e7a9469ce0e8c6f8e318246
carmtsang <carmen.tsang1208@hotmail.com>
2022-06-29 15:41:09 -0700

    add more likes

--- db/seeds/04_likes.sql
+++ db/seeds/04_likes.sql
@@ -3,3 +3,6 @@ INSERT INTO likes (user_id, map_id) VALUES (1, 1);
 INSERT INTO likes (user_id, map_id) VALUES (2, 1);
 INSERT INTO likes (user_id, map_id) VALUES (3, 2);
 INSERT INTO likes (user_id, map_id) VALUES (2, 4);
+INSERT INTO likes (user_id, map_id) VALUES (1, 3);
+INSERT INTO likes (user_id, map_id) VALUES (1, 2);
+INSERT INTO likes (user_id, map_id) VALUES (1, 6);
