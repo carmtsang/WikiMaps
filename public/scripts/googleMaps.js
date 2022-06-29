45b93386d0bce271f8186ed03042d2d3e8223fdb
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-29 21:42:35 +0000

    changed to focus on vancouver, added submit form

--- public/scripts/googleMaps.js
+++ public/scripts/googleMaps.js
@@ -2,16 +2,16 @@ const { marker } = require("leaflet");
 
 // initialzie map onto the page
 function initMap() {
-  const metro = { lat:49.2276, lng:123.0076 };
+  const metro = { lat:49.278707132806545, lng:-123.11010267860881 };
 
   const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4,
+    zoom: 10,
     center: metro,
 });
 
 
 const markerContent =
-'<form id="new-map-form" action="/maps" method="post"><label for="map-name">Map Name</label><input type="text" id="map-name" name="name" value="eg. Best Coffee in Town"><br><label for="map-description">What are you bookmarking?</label><textarea name="description" id="map-description"></textarea><button class="button-new"type="submit"><i class="fa-solid fa-location-dot"></i>Create a new Map <i class="fa-solid fa-map"></i></button></form>'
+'<form id="new-marker-form" action="/maps" method="post"><label for="marker-name">Title</label><input type="text" id="marker-name" name="name" value="eg. Best Coffee in Town"><br><label for="marker-description">Description</label><textarea name="description" id="marker-description"></textarea><button type="submit">Submit</button></form>'
 
 const infoWindow = new google.maps.InfoWindow({
   content: markerContent,
