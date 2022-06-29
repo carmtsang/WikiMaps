795900fd68070fc793811c5c0b2997d9ff3a42f5
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-29 02:50:26 +0000

    made changes in attempt to add pins

--- public/scripts/marker.js
+++ public/scripts/marker.js
@@ -6,15 +6,24 @@ let map0ptions = {
 // creating a map object
 let map = new L.map('map', map0ptions);
 
-// creating a layer object
+// creating a layer object - this shows the map
 let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
 
-// adding layer to the map
+// adding layer to the map - this is for the markers
 map.addLayer(layer);
 
-// creating a marker
-let marker = L.marker([49.238665712, -123.108999564]);
+let newMarker, markerLocation;
+$(function(){
+    L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
+        maxZoom: 19
+    }).addTo(map);
+    newMarkerGroup = new L.LayerGroup();
+    map.on('click', addMarker);
+});
 
-// adding marker to the map
-marker.bindPopup('Hello I am a PopUp').openPopup();
-marker.addTo(map);
+function addMarker(e){
+    // Add marker to map at click location; add popup window
+    let newMarker = new L.marker(e.latlng).addTo(map);
+}
+
