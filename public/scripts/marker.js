66397d8c26899b7baf5062e1e4751915b2262aec
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-29 21:13:42 +0000

    integrated googlemapsAPI, able to add markers with click

--- public/scripts/marker.js
+++ public/scripts/marker.js
@@ -12,25 +12,43 @@ let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
 // adding layer to the map - this is for the markers
 map.addLayer(layer);
 
-// $(function(){
-//     L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
-//         maxZoom: 19
-//     }).addTo(map);
-//     newMarkerGroup = new L.LayerGroup();
-//     map.on('click', addMarker);
-// });
-
-// function addMarker(e){
-//     // Add marker to map at click location; add popup window
-//     let newMarker = new L.marker(e.latlng).addTo(map);
-// }
+map.on('click', onMapClick);
 
 function onMapClick(e) {
-  document.getElementById('lat-lng').value = e.latlng.toString();
+  let geojsonfeature = {
+    "type": "Feature",
+    "properties": {},
+    "geometry": {
+          "type": "Point",
+          "coordinates": [e.latlng.lat, e.latlng.lng]
+    }
+  }
 
-  popup
-      .setLatLng(e.latlng)
-      .setContent("Copy and Paste into the Form \n" + e.latlng.toString())
-      .openOn(mymap);
+  let marker;
+
+  L.geoJSON(geojsonfeature, {
+    pointToLayer: function(feature, latlng) {
+
+      marker = L.marker(e.latlng, {
+        title: "Resource Location",
+        alt: "Resource Location",
+        riseOnHover: true,
+        draggable: true,
+      }).bindPopup("<form id='new-marker-form'><label for='marker-title'>Title</label><input type='text' id='marker-title' name='name' value='Best Coffee in Town'><br><lable for='marker description'>Description</lable><textarea name='description' id='marker-description'></textarea><input type='submit' value='SUBMIT' class='marker-submit-button'/></input><input type='button' value='DELETE' class='marker-delete-button'/></form>"
+
+        );
+
+      marker.on("popupopen", onPopUpOpen);
+
+      return marker;
+    }
+  }).addTo(map);
+
+  function onPopUpOpen() {
+    let tempMarker = this;
+
+    $(".marker-delete-button:visible").click(function () {
+      map.removeLayer(tempMarker);
+    });
+  }
 }
