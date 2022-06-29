cc593d655d580d2b541663a08eab130be3398f4b
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-06-29 21:29:43 +0000

    added info window on marker, and added form

--- public/scripts/googleMaps.js
+++ public/scripts/googleMaps.js
@@ -1,3 +1,6 @@
+const { marker } = require("leaflet");
+
+// initialzie map onto the page
 function initMap() {
   const metro = { lat:49.2276, lng:123.0076 };
 
@@ -6,13 +9,33 @@ function initMap() {
     center: metro,
 });
 
+
+const markerContent =
+'<form id="new-map-form" action="/maps" method="post"><label for="map-name">Map Name</label><input type="text" id="map-name" name="name" value="eg. Best Coffee in Town"><br><label for="map-description">What are you bookmarking?</label><textarea name="description" id="map-description"></textarea><button class="button-new"type="submit"><i class="fa-solid fa-location-dot"></i>Create a new Map <i class="fa-solid fa-map"></i></button></form>'
+
+const infoWindow = new google.maps.InfoWindow({
+  content: markerContent,
+});
+
+// creates markers when clicked on page
 map.addListener("click", (e) => {
 
   const marker = new google.maps.Marker({
     position: e.latLng,
     map: map,
   });
+
+  marker.addListener("click", (e) => {
+    infoWindow.open({
+      anchor: marker,
+      map,
+      shouldFouces: false,
+    });
   });
+
+});
+
+
 };
 
 window.initMap = initMap;
