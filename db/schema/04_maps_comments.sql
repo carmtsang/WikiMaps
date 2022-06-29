5b11dc822a8280f086c16258f0069ea9128bd4cf
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-29 00:15:41 +0000

    move create new map to /users

--- db/schema/04_maps_comments.sql
+++
@@ -1,9 +0,0 @@
--- Stretch goal to add comments to a map
-
-DROP TABLE IF EXISTS map_comments CASCADE;
-CREATE TABLE map_comments (
-  id SERIAL PRIMARY KEY NOT NULL,
-  map_id INTEGER REFERENCES maps(id) on DELETE CASCADE,
-  commenter_id INTEGER REFERENCES user(id),
-  comment TEXT,
-);
