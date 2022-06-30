5a013c4dd0373080540a933a50a12d8bf1e5c5e1
roop <rplawn@icloud.com>
2022-06-29 18:25:18 -0700

    created separate routes for login and log out, adding automatic cookie and replaced users/login with /login for readability

--- routes/users.js
+++ routes/users.js
@@ -7,26 +7,13 @@
 
 const express = require('express');
 const router  = express.Router();
-const cookieParser = require('cookie-parser')
+// const cookieParser = require('cookie-parser')
 
-// const { users } = require('../constants');
 
 module.exports = (db) => {
-  router.get('/', (req, res) => {
-    res.render('users', { userID: req.cookies.user_id });
-  })
-
-  router.get('/login/:user_id', (req, res) => {
-    res.cookie('user_id', req.params.user_id);
-    res.redirect('/');
-  })
-
   return router;
-
 };
 
-
-// make a post for each
 
 
 
