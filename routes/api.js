00d46e1370e1a42418d5c311d3cf65553cada445
carmtsang <carmen.tsang1208@hotmail.com>
2022-06-29 21:49:23 -0700

    add api route for user favourite maps

--- routes/api.js
+++ routes/api.js
@@ -2,27 +2,47 @@ const express = require('express');
 const router  = express.Router();
 const cookieParser = require('cookie-parser')
 
+const { getUserMadeMaps } = require('../user-helpers');
 
 
-const getUserMadeMaps = (userID, db) => {
-  const queryString = `SELECT * FROM maps WHERE owner_id = $1`
-  return db
-    .query(queryString, [userID])
+const selectUserFav = (userID, db) => {
+  const queryString = `SELECT * FROM favourites WHERE user_id = $1`
+  return db.query(queryString, [userID])
     .then((res) => {
-      return res.rows})
-    .catch(err => console.error(err.stack))
+      return res.rows[0];
+    })
 }
 
 module.exports = (db) => {
+
+  router.get('/user/favourites', (req, res) => {
+    const userID = req.cookies.user_id;
+    selectUserFav(userID, db)
+    .then(userFave => {
+      res.json(userFave)
+    })
+    .catch(err => {
+    res
+      .status(500)
+      .json({ error: err.message });
+  });
+  })
+
+  // maps created by the user
   router.get('/user/maps', (req, res) => {
     const userID = req.cookies.user_id;
 
     getUserMadeMaps(userID, db)
       .then(userMaps => {
-        console.log(userMaps)
         res.json(userMaps)
       })
-  })
+      .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
 
   return router
 }
