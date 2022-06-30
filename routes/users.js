313c2f857b0097817ab33790fe76d589eca69a3e
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-30 20:37:19 +0000

    delete unneccessary code

--- routes/users.js
+++ routes/users.js
@@ -17,6 +17,7 @@ module.exports = (db) => {
   router.get('/', (req, res) => {
     const userID = req.cookies.user_id;
 
+
     getUser(userID, db)
       .then(user => {
         const templateVars = { userID: user }
