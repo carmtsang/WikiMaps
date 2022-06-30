98e6e4924fc4a9150b304c9842453d5d3479ea79
Carmen Tsang <carmtsang89@hotmail.com>
2022-06-30 16:08:41 +0000

    add functions to load likes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,35 +1,69 @@
 // Client facing scripts here
 
+// const { load } = require("dotenv");
 
 $(() => {
 
-const renderUserMaps = userMaps => {
-  for (const map of userMaps) {
-    let $post = createUserMaps(map);
-    $('#my-maps').append($post);
+    // add user created maps
+  const renderUserMaps = userMaps => {
+    for (const map in userMaps) {
+      let $post = createUserMaps(map);
+      $('#my-maps').append($post);
+      console.log('in renderUserMaps')
+    }
+  };
+
+  const createUserMaps = userMaps => {
+    return ` <div class="user_map_list">
+    <div class="map-name-description">
+    <a href="/maps/:${userMaps.id}">${userMaps.name}</a>
+    <p>${userMaps.description} </p>
+    </div>
+    <form action="/maps/:${userMaps.id}/delete" method="post">
+      <button class="button" type="submit"><i class="fa-solid fa-trash-can"></i></button>
+    </form>
+    </div>
+    `
   }
-};
+
+  const loadUserMaps = () => {
+    $.ajax('/api/user/maps', { method: 'GET' })
+      .then(userMaps => renderUserMaps(userMaps))
+      .catch(error => console.log(error));
+  }
+
+  loadUserMaps()
+
+  // add user likes
+  renderUserLikes = userLikes => {
+    for (const like in userLikes) {
+      let $post = addLikes(like);
+      $('#my-likes').append($post);
+    }
+  }
+
+  const addLikes = likes => {
+    return `<div class="user_map_list">
+    <div class="map-name-description">
+      <a href="/maps/${likes.id}">${likes.name}</a>
+      <p>${likes.description}</p>
+    </div>
+    <form action="/likes/:delete" method="post">
+      <button class="button" type="submit"> <i class="fa-solid fa-heart"></i></button>
+    </form>
+    </div>
+    `
+  }
+
+  const loadLikes = () => {
+    $.ajax('/api/user/likes', { method: 'GET' })
+      .then(userLikes => renderUserLikes(userLikes))
+      .catch(error => console.log(error));
+  }
 
 
-const createUserMaps = userMaps => {
-  return ` <div class="user_map_list">
-  <div class="map-name-description">
-  <a href="/maps/:${userMaps.id}">${userMaps.name}</a>
-  <p>${userMaps.description} </p>
-</div>
-<form action="/maps/:${userMaps.id}/delete" method="post">
-  <button class="button" type="submit"><i class="fa-solid fa-trash-can"></i></button>
-</form>
-</div>
-  `
-}
 
-const loadUserMaps = () => {
-  $.ajax('/api/user/maps', { method: 'GET' })
-    .then(userMaps => renderUserMaps(userMaps))
-    .catch(error => console.log(error));
-}
-
-loadUserMaps()
+  loadUserMaps();
+  loadLikes();
 
 });
