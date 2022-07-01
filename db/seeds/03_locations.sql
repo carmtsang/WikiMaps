5c00bc894e130facc9b337ece3c7f56ae00654ba
Carlo Tanjuaquio <carlotanjuaquio@gmail.com>
2022-07-01 05:22:30 +0000

    debugged databases, paired programming with carmen

--- db/seeds/03_locations.sql
+++ db/seeds/03_locations.sql
@@ -1,26 +1,26 @@
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES ( 49.255615005654924, -123.11507050661429, 'JJ Bean', 'decent coffee', 1, 1);
+   owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (1, 1, 49.255615005654924, -123.11507050661429, 'JJ Bean', 'decent coffee', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.284256509181766, -123.10930776074136, 'Timber Train', 'great poor over coffee', 1, 1);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (1, 1, 49.284256509181766, -123.10930776074136, 'Timber Train', 'great poor over coffee', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.281792317336965, -123.08540391479706, 'Prototype Coffee', 'nice spot', 2, 1);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (2, 1, 49.281792317336965, -123.08540391479706, 'Prototype Coffee', 'nice spot', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.29680744650704, -123.13565840392819, 'Stanley Park', '50/50', 3, 2);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (3, 2, 49.29680744650704, -123.13565840392819, 'Stanley Park', '50/50', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.16864715256868, -122.57992336032277, 'Fort Langley', 'X-Files', 3, 2);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (3, 2, 49.16864715256868, -122.57992336032277, 'Fort Langley', 'X-Files', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.115650042872, -123.113817219568, 'Finn Slough', 'Old woold houses floating or built on stilts', 3, 4);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (3, 4, 49.115650042872, -123.113817219568, 'Finn Slough', 'Old woold houses floating or built on stilts', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.33943349144921, -123.26059048207361, 'Lighthouse Park', 'not very hidden, but still good', 2, 4);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (2, 4, 49.33943349144921, -123.26059048207361, 'Lighthouse Park', 'not very hidden, but still good', 'https://http.cat/100');
 INSERT INTO locations (
-  longitude, latitude, name, description, owner_id, map_id)
-  VALUES (49.21680474420945, -123.20595924647859, 'Iona Beach', 'Gorgeous palce to watch the sunset', 2, 4);
+  owner_id, map_id, longitude, latitude, name, description, url)
+  VALUES (2, 4, 49.21680474420945, -123.20595924647859, 'Iona Beach', 'Gorgeous palce to watch the sunset', 'https://http.cat/100');
 
 
