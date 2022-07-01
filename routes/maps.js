896081f5e8dd31d896899c568fe322dc62a8c0d6
Carmen Tsang <carmtsang89@hotmail.com>
2022-07-01 03:14:37 +0000

    remove unneccessary code

--- routes/maps.js
+++ routes/maps.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router  = express.Router();
-const { addMap, getUser, getMapById } = require('../user-helpers');
+const { addMap, getMapById } = require('../user-helpers');
 
 
 module.exports = (db) => {
@@ -8,36 +8,25 @@ module.exports = (db) => {
   // renders the individual map
   router.get("/:map_id", (req, res) => {
     const userID = req.cookies.user_id;
-    const map_id = req.params.map_id
+    const map_id = req.params.map_id;
     console.log(req.params)
     getMapById(map_id, db)
       .then(map => {
         const templateVars = { userID, map };
         res.render('maps', templateVars);
       })
-
-    // db.query(`SELECT * FROM maps;`)
-    //   .then(data => {
-    //     const users = data.rows;
-    //     res.json({ users });
-    //   })
-    //   .catch(err => {
-    //     res
-    //       .status(500)
-    //       .json({ error: err.message });
-    //   });
   });
 
 
     // posting a new map
     router.post('/', (req, res) => {
       const userID = req.cookies.user_id;
-      const map = req.body
+      const map = req.body;
 
       addMap(userID, map, db)
-        .then(res => res.rows)
-
-      res.redirect('users')
+        .then(() => {
+          res.redirect('users');
+        })
     })
 
   return router;
