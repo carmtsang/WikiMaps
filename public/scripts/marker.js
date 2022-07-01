c461d565638bcd0c04952e2cf4f81741fb7a5770
Carmen Tsang <carmtsang89@hotmail.com>
2022-07-01 17:06:52 +0000

    load json of map  locations to indiv map

--- public/scripts/marker.js
+++ public/scripts/marker.js
@@ -1,15 +1,13 @@
 const pathname = window.location.pathname.split('/')
-const mapID = pathname[2]
+const mapId = pathname[2]
 
-console.log(mapID)
+
 $(() => {
+  const loadMarkers = () => {
+    return $.get(`/api/all_markers/${mapId}`)
 
-
-  // const loadMarkers = () => {
-  //   $.ajax('/api/all_markers/${mapID}', { method: GET })
-  //     .then(res => console.log(res))
-  // }
-  // loadMarkers()
+  }
+  console.log(loadMarkers())
 
 })
 
