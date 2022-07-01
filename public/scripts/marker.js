e3901ffec4dff67d92a612451a885135af06f877
Carmen Tsang <carmtsang89@hotmail.com>
2022-07-01 16:56:19 +0000

    get mapid from window path

--- public/scripts/marker.js
+++ public/scripts/marker.js
@@ -1,1 +1,16 @@
+const pathname = window.location.pathname.split('/')
+const mapID = pathname[2]
+
+console.log(mapID)
+$(() => {
+
+
+  // const loadMarkers = () => {
+  //   $.ajax('/api/all_markers/${mapID}', { method: GET })
+  //     .then(res => console.log(res))
+  // }
+  // loadMarkers()
+
+})
+
 
