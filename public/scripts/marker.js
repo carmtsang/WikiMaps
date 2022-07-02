0e4014560aca451bc557cb5e41dd9fc6996a5d7c
Carmen Tsang <carmtsang89@hotmail.com>
2022-07-02 04:00:41 +0000

    delete unneccessary code, refractor mapID

--- public/scripts/marker.js
+++ public/scripts/marker.js
@@ -1,36 +1,3 @@
-
-const path = window.location.pathname.split('/')
-const mapId = path[2]
+const mapId = window.location.pathname.split('/')[2]
 
 
-
-// const showPins = pins => {
-//   for (let pin of pins) {
-//     let $coords = latlong(pin);
-//     console.log($coords)
-//     $('#test').prepend($coords)
-//   }
-// }
-
-// const latlong = coord => {
-//   return `<p>{ lat: ${coord.longitude}, lng: ${coord.latitude} }</p>`
-// };
-
-
-// const getMapCoords = () => {
-//   $.ajax(`/api/all_markers/${mapId}`, { method: 'GET' })
-//     .then(pins => showPins(pins))
-// }
-
-// getMapCoords()
-
-
-
-
-
-// const path = window.location.pathname.split('/')
-// const mapId = path[2]
-
-
-
-// initialzie map onto the page
